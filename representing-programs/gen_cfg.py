0c13c2fcc8e77b2a3a2836e48ee5e3a7fce6b7d5
Nikil-Shyamsunder <nikil.shyamsunder@gmail.com>
2025-09-08 23:40:24 -0400

    fix gen_cfg to work on multi-function bril files properly

--- representing-programs/gen_cfg.py
+++ representing-programs/gen_cfg.py
@@ -2,44 +2,43 @@ import json
 import sys
 from collections import defaultdict
 
-def gen_basic_blocks(ir):
+def gen_basic_blocks(function):
     basic_blocks = []
     curr_block = []
 
-    for function in ir['functions']:
-        for instr in function['instrs']:
-            if 'label' in instr:
-                if curr_block:
-                    basic_blocks.append(curr_block)
-                curr_block = [instr]
-            elif 'op' in instr and instr['op'] in ['br', 'jmp', 'ret']:
-                curr_block.append(instr)
+    for instr in function['instrs']:
+        if 'label' in instr:
+            if curr_block:
                 basic_blocks.append(curr_block)
-                curr_block = []
-            else:
-                curr_block.append(instr)
-        if curr_block:
+            curr_block = [instr]
+        elif 'op' in instr and instr['op'] in ['br', 'jmp', 'ret']:
+            curr_block.append(instr)
             basic_blocks.append(curr_block)
             curr_block = []
+        else:
+            curr_block.append(instr)
+    if curr_block:
+        basic_blocks.append(curr_block)
+        curr_block = []
 
     return basic_blocks
 
 
-def get_label(basic_blocks, idx):
+def get_label(basic_blocks, func, idx):
     block = basic_blocks[idx]
     if 'label' in block[0]:
         label = block[0]['label'] 
     else:
-        label = f"block{idx}"
+        label = f"{func}-block{idx}"
     
     return label
 
 
-def build_cfg(basic_blocks):
+def build_cfg(func_name, basic_blocks):
     cfg = {}
     
     for i, basic_block in enumerate(basic_blocks):
-        label = get_label(basic_blocks, i)
+        label = get_label(basic_blocks, func_name, i)
 
         next = []
         if 'op' not in basic_block[-1]: # we have a label with nothing after
@@ -51,25 +50,25 @@ def build_cfg(basic_blocks):
             elif op == 'ret':
                 pass # next should stay empty
             elif i + 1 < len(basic_blocks): # there is a next block
-                next = [get_label(basic_blocks, i + 1)]
+                next = [get_label(basic_blocks, func_name, i + 1)]
             else:
                 pass # next should stay 
             
         cfg[label] = next
     
-    return cfg
+    return cfg.items()
 
 
 if __name__ == "__main__":
     # load json from stdin
     bril = json.load(sys.stdin)
-    basic_blocks = gen_basic_blocks(bril)
-
-    # for basic_block in basic_blocks:
-    #     print(basic_block)
-    #     print()
-    # print(basic_blocks)
-    cfg = build_cfg(basic_blocks)
+    
+    
+    cfg = []
+    for func in bril['functions']:
+        basic_blocks = gen_basic_blocks(func)
+        func_cfg = build_cfg(func['name'], basic_blocks)
+        cfg += func_cfg
     print(cfg)
     # print(cfg)
 
