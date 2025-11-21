a60a5c0a5c96527fefdfd7ce2018393ee0716ec0
Nikil-Shyamsunder <nikil.shyamsunder@gmail.com>
2025-11-21 01:56:06 -0500

    fix trace generation to handle new bril interp trace format

--- src/speculative-opt/gen_traces.sh
+++ src/speculative-opt/gen_traces.sh
@@ -1,9 +1,6 @@
 for f in ../../../bril/benchmarks/core/*.bril; do
     basename="${f##*/}"
-    o="traces/${basename%.bril}.trc"
+    o="raw_traces/${basename%.bril}.trc"
     ARGS=$(sed -n 's/^[[:space:]]*# ARGS: \(.*\)/\1/p' "$f")
-    TFILE="/tmp/$$.trace"
-    bril2json < "$f" | brili $ARGS | grep trace: > $TFILE
-    cut -d':' -f2- $TFILE | head -n 10 > "$o"
-    rm $TFILE
+    bril2json < "$f" | brili --trace $ARGS 2>&1 | grep "trace" > "$o"
 done
